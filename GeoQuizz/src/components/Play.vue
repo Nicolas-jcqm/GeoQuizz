cd05a647fb97e381d3776d7a959eb979d2aa5a32
Marquant Lucas <lucas.marquant2@etu.univ-lorraine.fr>
2018-02-05 18:18:56 +0100

    ajout de la carte et modif store et component

--- GeoQuizz/src/components/Play.vue
+++ GeoQuizz/src/components/Play.vue
@@ -1,15 +1,22 @@
 <template>
+
   <div class="play">
         <div class="windows">
-            <p>question actuel : {{ question_actuel }} / 10</p>
-            <p>{{ name }}</p>
-            <div class="image"></div>
-            <div class="map"></div>
+            <p id='question'>question actuel : {{ question_actuel }} / 10</p>
+            <div class="image"><img src="http://via.placeholder.com/500x500"></div>
+            <div id="map">
+                <v-map :zoom=13 :center="[47.413220, -1.219482]">
+                <v-tilelayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></v-tilelayer>
+                <v-marker :lat-lng="[47, 0]"></v-marker>
+                </v-map>
+            </div>
         </div>
   </div>
 </template>
 
 <script>
+    import Vue from 'vue'
+    import Vue2Leaflet from 'vue2-leaflet'
     import api from '@/api'
     import Vuex from 'vuex'
     import {
@@ -19,10 +26,14 @@
         mapState
     } from 'vuex'
 
+    Vue.component('v-map', Vue2Leaflet.Map);
+    Vue.component('v-tilelayer', Vue2Leaflet.TileLayer);
+    Vue.component('v-marker', Vue2Leaflet.Marker);
+
     export default {
 
         computed: {
-            ...mapState(['name'])
+            ...mapState(['name', 'question_actuel'])
         },
 
         created() {
@@ -35,7 +46,29 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-
+<style>
+    @import "../../node_modules/leaflet/dist/leaflet.css";
+    
+    #map{
+        width: 500px;
+        height: 500px;
+        margin: auto;
+    }
+    
+    .image{
+        width: 500px;
+        height: 500px;
+        margin: auto;
+    }
+    
+    .windows{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+    
+    #question{
+        width: 100%;
+    }
 
 </style>
