144eb8b522c0cf20806d4efb0a13369fbeb5225e
thibaudG <thibaud.grepin@gmail.com>
2018-02-10 05:23:22 +0100

    base

--- GeoQuizzAdmin/src/components/Serie.vue
+++ GeoQuizzAdmin/src/components/Serie.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-    <h1> {{series.filter( (ser) => ser._id === idSeries)[0].nom}}</h1>
+    <h1> {{series}}</h1>
+    <form @submit.prevent="putSerie()" class="form-sign">
+        <label>Nom de la série : </label>
+        <input ref="nom" :value="series.nom" />
+        <label>Latitude : </label>
+        <input ref="latitude" :value="series.latitude" />
+        <label>Longitude : </label>
+        <input ref="longitude" :value="series.longitude" />
+        <label>Distance : </label>
+        <input ref="distance" :value="series.distance" />
+        <label>Ville : </label>
+        <input ref="ville" :value="series.ville" />
+        <label>Zoom : </label>
+        <input ref="zoom" :value="series.zoom" />
+        <br>
+        <br>
+        <input type="submit"class="btn btn-5" value="Modifier une des valeurs"></input>
+      </form>
   </div>
 </template>
 <script>
@@ -15,12 +32,18 @@
     created(){
       this.idSerie=this.$route.params.id
       console.log("test:"+this.idSerie);
-      alert("test");
+      //alert("test");
       api.get("/series/"+this.idSerie).then((response)=>{
       this.series=response.data
       }).catch((error) =>{
       alert("La serie auquel vous essayez d'accéder n'existe pas !")
       })
+    },
+    methods : {
+      putSerie(){
+        console.log(this.$refs)
+        //api.put("/series/"+this.idSerie)
+      }
     }
   }
 </script>
